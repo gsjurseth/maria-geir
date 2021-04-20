0b26b09bfa3efe4d7884507ac9464e745a8eba66
Geir Sjurseth <gsjurseth@google.com>
2021-04-20 15:12:39 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # maria-geir
+Sample repo showin Envoy used for several services plus a portal that documents them
+
+## What's in here anyway?
+The following have been setup and are run with docker compose
+* grpc server written in java running greeter service: normal and pirate mode available
+* grpc client written in golang that calls the remote service (and accepts an optional key as an argument)
+* a graphql service. This uses all flat files and is simply the book list example. It's self contained
+* an envoy service that proxies for all of it.
+
+## What's coming
+I'm in the process of building out a few more things
+* an async api documentation example to be published in the portal
+* a kafka image that will auto-create an example topic
+* a producer for said topic that spits out messages once per second
+* a consumer that subscribes to said topic and then pushes those messages down a websocket once they connect
+* a websocket client that can receive these messages
+* documentation published in the portal for that same websocket service
+
+The idea here is to create working example where apigee is used in concert with a simple microservice
+to translate kafka -> websockets for easier consumption by external parties.
+
+## Getting setup
+Run the following:
+
+NB:
+* The email will be used to create the developer and so needs to be a user with rights. Probably your own email (no password is used)
+* The remote url is probably the base url of your Apigee X setup. If you've used the auto-provisioner it will be one that ends in `nip.io`
+
+```bash
+./setup.sh -o your-org -e your-environment -t $(gcloud beta auth print-access-token) -r "http://remote-url-for-apigee-x" -u user@email.com
+```
+
+
+## Building and Runnig it once configured
+To get this running you'll need to build it all (after you've run setup mind you) by doing:
+
+```bash
+docker-compose build
+```
+
+Once it's built you can play with any of the config files and environment variables named
+in `docker-compose.yaml`
+
+Finally you can run it by:
+
+```bash
+docker-compose up
+```
---
+++ setup.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+config=${BASEDIR}/config.yaml
+
+usage() { 
+  echo "Usage assumes setup for Apigee X. You may need to edit this for hybrid and definitely for legacy/opdk" 1>&2
+  echo "Usage: $0 [-h] [-u <user-email: an email address for the account>] [-o <organiztion> ] [-e <environment>] [-t <token>] [-r <remote url>] [-c <config-file-path]" 1>&2
+  exit 1;
+}
+
+while getopts ":e:o:t:r:c:u:h" o; do
+    case "${o}" in
+        h)
+          usage
+        ;;
+        u)
+            email=${OPTARG}
+        ;;
+        e)
+            env=${OPTARG}
+        ;;
+        o)
+            org=${OPTARG}
+        ;;
+        t)
+            token=${OPTARG}
+        ;;
+        r)
+            remoteurl=${OPTARG}
+        ;;
+        c)
+            config=${OPTARG}
+        ;;
+        *)
+            usage
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${env}" ] || [ -z "${org}" ] || [ -t "${token}" ] || [ -z "${remoteurl}" ] || [ -z "${email}" ]; then
+    usage
+fi
+
+BASEURL=https://apigee.googleapis.com/v1/organizations/${org}
+
+OS=$(uname -s)
+
+case $OS in
+  "Linux")
+    arch="linux"
+  ;;
+  "Darwin")
+    arch="macOS"
+  ;;
+esac
+
+wget "https://github.com/apigee/apigee-remote-service-cli/releases/download/v2.0.0/apigee-remote-service-cli_2.0.0_${arch}_64-bit.tar.gz" -O apigee-remote-service-cli.tar.gz
+wget "https://github.com/apigee/apigee-remote-service-envoy/releases/download/v2.0.0/apigee-remote-service-envoy_2.0.0_${arch}_64-bit.tar.gz" -O apigee-remote-service-envoy.tar.gz
+
+
+
+tar xf apigee-remote-service-cli.tar.gz apigee-remote-service-cli
+tar xf apigee-remote-service-envoy.tar.gz apigee-remote-service-envoy
+
+
+${BASEDIR}/apigee-remote-service-cli provision -o $org -e $env -t $token --runtime $remoteurl > $config
+
+# Our api product
+curl -X POST "${BASEURL}/apiproducts" -H "Authorization: Bearer $token" -H "Content-Type: application/json" -d @${BASEDIR}/apigee-jsons/apiproduct.json > ${BASEDIR}/my_apiproduct.json
+
+# Our developer
+user=$(echo gsjurseth@google.com | sed -e 's/\(.*\)@.*/\1/')
+cat $BASEDIR/apigee-jsons/developer.json | sed -e "s/@@EMAIL@@/${email}/" -e "s/@@USER@@/${user}/" | curl -X POST "${BASEURL}/developers" -H "Authorization: Bearer $token" -H "Content-Type: application/json" -d @- > ${BASEDIR}/my_developer.json
+
+# Our app
+curl -X POST "${BASEURL}/developers/${email}/apps" -H "Authorization: Bearer $token" -H "Content-Type: application/json" -d @${BASEDIR}/apigee-jsons/app.json > ${BASEDIR}/my_app.json
